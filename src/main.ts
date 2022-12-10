3661d6c4e627865b5a1c5f9c1e87d72f410b45c6
Raphael <rxphxel7k@gmail.com>
2022-12-09 22:24:06 -0300

    docs

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(8080);
 }
 bootstrap();
